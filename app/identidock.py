b74b8c2143cfb42fa542a3e25880eb769059d018
Steve Hurlock <steven_hurlock@comcast.com>
2018-06-10 18:41:06 -0400

    changes to add the dnmonster image, page 90

--- app/identidock.py
+++ app/identidock.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
+
 app = Flask(__name__)
 default_name = 'Joe Bloggs'
 
@@ -14,18 +16,20 @@ def mainpage():
              <input type="submit" value="submit">
              </form>
              <p>You look like a: 
-             <img src"/monster/monster.png"/>
+             <img src="/monster/monster.png"/>
              '''.format(name)
    footer = '</body></html>'
 
    return header + body + footer
 
+@app.route('/monster/<name>')
+def get_identicon(name):
+   r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+   image = r.content;
+
+   return Response(image, mimetype='image/png')
+
 if __name__ == '__main__': 
-   app.run(debug=True, host='0.0.0.0')
-
-
-
-if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0')
 
 
